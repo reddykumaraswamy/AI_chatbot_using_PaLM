634fab122389adf8e7c8d6a865880072ed01fc07
Reddy Kumaraswamy <reddy.kumaraswamy15@gmail.com>
2023-10-14 12:17:39 +0530

    first commit

---
+++ PaLMBot.py
@@ -0,0 +1,32 @@
+# !pip install -q google-generativeai
+
+import pprint
+import google.generativeai as palm
+
+palm.configure(api_key='AIzaSyDZcTSeA8OoyyHu6yCZtfo0BjytgpBlOiU')
+
+models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+model = models[0].name
+# print(model)
+
+def chat_with_palm(prompt):
+    response = palm.generate_text(
+        model=model,
+        prompt=prompt,
+        temperature=0,
+        # The maximum length of the response
+        max_output_tokens=800,
+    )
+    return response.result
+
+
+print("Welcome to PaLM Bot! Type 'exit' to end the conversation.")
+while True:
+    user_input = input("You: ")
+
+    if user_input.lower() == 'exit':
+        print("PaLM Bot: Goodbye!")
+        break
+
+    response = chat_with_palm(user_input)
+    print("PaLM Bot:", response)
